bd2c07cab927e4c65f21a0fa9caddb516f3afa25
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro.local>
2021-09-01 17:29:18 -0700

    addressing feedback

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -2,23 +2,23 @@ import interactiveColumns from "./interactiveColumns";
 import staffBios from "./staffBios";
 import splashPage from "./splash-page";
 import projectGrid from "./projectGrid";
+import stickyHeader from "./sticky-header";
 
 (function () {
   const ic = interactiveColumns();
   const sb = staffBios();
   const sp = splashPage();
   const pg = projectGrid();
+  const sh = stickyHeader();
 
   ic.init();
   sb.init();
   sp.init();
   pg.init();
+  sh.init();
 
   if (window.location.href.indexOf("#available-now") >= 1) {
     pg.loadLeasing();
   }
+
 })();
-
-// (function() {
-
-// })();
---
+++ src/js/sticky-header.js
@@ -0,0 +1,46 @@
+const stickyHeader = function () {
+  function init() {
+
+    var headerScrollHook = document.getElementById("header-scroll-up");
+    var breakpoint = 1200;
+    var prevScrollpos = window.pageYOffset;
+    var browserHeight = window.innerHeight;
+
+    window.onscroll = function () {
+         /* When the user scrolls down, hide the header. When the user scrolls up, show the header*/
+    
+
+      if (window.innerWidth > breakpoint) {
+        var currentScrollPos = pageYOffset;
+        
+        if (currentScrollPos > browserHeight / 4) {
+          if (prevScrollpos > currentScrollPos) {
+            if (!headerScrollHook.classList.contains("sticky-visible")) {
+              headerScrollHook.classList.add("sticky-visible");
+              headerScrollHook.classList.remove("sticky-hide");
+            }
+          } else {
+            if (!headerScrollHook.classList.contains("sticky-hide")) {
+              headerScrollHook.classList.remove("sticky-visible");
+              headerScrollHook.classList.add("sticky-hide");
+            }
+          }
+        } else {
+          headerScrollHook.classList.remove("sticky-visible");
+          headerScrollHook.classList.remove("sticky-hide");
+        }
+        prevScrollpos = currentScrollPos;
+      }
+
+      else {
+          headerScrollHook.style.position = 'relative';
+      }
+    };
+  }
+
+  return {
+    init: init,
+  };
+};
+
+export default stickyHeader;
