65dc19595897fa4ae4f0bdc29aaf41777406fa12
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro.local>
2021-10-07 19:17:21 -0400

    adding tabindex

--- src/js/interactiveColumns.js
+++ src/js/interactiveColumns.js
@@ -10,6 +10,7 @@ const interactiveColumns = (function () {
             var interactiveColumns = document.querySelectorAll('.interactive-callout-column');
     
             interactiveColumns.forEach(el => {
+                el.tabIndex = 0;
                 el.addEventListener('click', function(e) {
                     e.preventDefault();
                     tc.toggle(this);
