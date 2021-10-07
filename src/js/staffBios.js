abb7e8e8b4cfd26db46917766bd7e98488ae704d
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro.local>
2021-10-07 19:57:27 -0400

    adding tabindex to staff bios

--- src/js/staffBios.js
+++ src/js/staffBios.js
@@ -6,6 +6,7 @@ const staffBios = function () {
   function addEventListeners() {
     var bios = document.querySelectorAll(".staff-bio");
     bios.forEach((el) => {
+      el.tabIndex = 0;
       if (Boolean(el.querySelector(".staff-bio-expand img"))) {
         el.querySelector(".staff-bio-expand img").addEventListener(
           "click",
